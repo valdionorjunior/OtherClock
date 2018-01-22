d8fd7f609a8962bf4e492ed60406011c351c797b
valdionorjunior <valdionorjunior@outlook.com>
2018-01-22 17:16:08 -0200

    Adicionado view de data com atualização igual ao relogio

--- app/src/main/java/br/com/rodrigues/junior/otherclock/MainClock.java
+++ app/src/main/java/br/com/rodrigues/junior/otherclock/MainClock.java
@@ -3,14 +3,19 @@ package br.com.rodrigues.junior.otherclock;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class MainClock extends AppCompatActivity {
+public class MainClock extends AppCompatActivity implements View.OnClickListener{
 
     private TextView myClock;
+    private TextView myDate;
+    private TextView mySelectTimeZone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +23,21 @@ public class MainClock extends AppCompatActivity {
         setContentView(R.layout.activity_main_clock);
 
         myClock = (TextView) findViewById(R.id.tv_clock);
+        myDate = (TextView) findViewById(R.id.tv_date);
+        mySelectTimeZone = (TextView) findViewById(R.id.tv_select_time_zone);
 
         final Handler upRealTime = new Handler();
 
         upRealTime.post(new Runnable() {
             @Override
             public void run() {
-                myClock.setText(updateTime());
+                myClock.setText(updateTime("HH:mm:ss"));
+                myDate.setText(updateTime("E. dd 'de' MMMM 'de' yyyy"));
                 upRealTime.postDelayed(this,100);
             }
         });
+
+        mySelectTimeZone.setOnClickListener(this);
 
     }
 
@@ -35,16 +45,26 @@ public class MainClock extends AppCompatActivity {
         super();
     }
 
-    private String updateTime() {
+    private String updateTime(String simpleFormat) {
 
         Calendar calendar = new GregorianCalendar();
+        DateFormat df;
 
-        int h = calendar.get(Calendar.HOUR_OF_DAY);
-        int m = calendar.get(Calendar.MINUTE);
-        int s = calendar.get(Calendar.SECOND);
+        df = new SimpleDateFormat(simpleFormat);
 
-        return h+":"+m+":"+s;
+        //int h = calendar.get(Calendar.HOUR_OF_DAY);
+        //int m = calendar.get(Calendar.MINUTE);
+        //int s = calendar.get(Calendar.SECOND);
+
+        //return h+":"+m+":"+s;
+
+        return String.valueOf(df.format(calendar.getTime()));
+
 
     }
 
+    @Override
+    public void onClick(View v) {
+        mySelectTimeZone.setText("Anda não é o que eu quero");
+    }
 }
